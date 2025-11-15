b954b64788080cf4b52cf13d72d4cc9130d8e474
Afton Gauntlett <afton.gauntlett@gmail.com>
2025-11-15 16:13:05 -0500

    chore: update shared components barrel exports
    Add exports for all new shared UI components to components/shared/index.ts

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -5,3 +5,17 @@
 
 // Re-export shared types
 export type { BaseRecommendation, FriendSummary, SortOption } from "./types";
+
+// Re-export new reusable UI components
+export { default as StarRating } from "./StarRating";
+export { default as Accordion } from "./Accordion";
+export { default as StatusBadge } from "./StatusBadge";
+export { default as PrivacyToggle } from "./PrivacyToggle";
+export { default as ActionButtonGroup } from "./ActionButtonGroup";
+export type { ActionConfig } from "./ActionButtonGroup";
+export { default as MetadataRow } from "./MetadataRow";
+export type { MetadataItem } from "./MetadataRow";
+export { default as GenreChips } from "./GenreChips";
+export { default as MediaPoster } from "./MediaPoster";
+export { default as MediaDetailModal } from "./MediaDetailModal";
+export { default as UnifiedMediaCard } from "./UnifiedMediaCard";
