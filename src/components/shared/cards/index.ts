4e28b06345d1f9d50b08b6c0203dea8eabceef10
Afton Gauntlett <afton.gauntlett@gmail.com>
2025-12-05 16:35:14 -0500

    refactor: remove deprecated MediaCard component
    Remove unused MediaCard component from shared cards. Media items are
    now rendered using MediaListItem and other specialized components,
    making this wrapper redundant.

--- src/components/shared/cards/index.ts
+++ src/components/shared/cards/index.ts
@@ -4,7 +4,6 @@
  * Export all specialized card components from a single import point
  */
 
-export { default as MediaCard } from "./MediaCard";
 export { default as JobCard } from "./JobCard";
 export { default as RecipeCard } from "./RecipeCard";
 export { default as BoardCard } from "./BoardCard";
